fff83b5a4da28f020aa8ec2b46630bf4a2effe5b
Stephanie Pearce <stephy.pearce129@gmail.com>
2025-08-26 17:39:09 +0100

    Edited the code to make cleaner:
    - There is now a singular function to convert the images (with specific file ext that user chooses) to JPG, rather than two functions to convert png -> jpg or heic -> jpg.
    - Removed rstudioapi package, and replaced the file directory selection function with a function from svDialogs package - compatible for cross-platform and outside RStudio

--- ALL_to_JPG_Converter.R
+++ ALL_to_JPG_Converter.R
@@ -3,78 +3,49 @@
 # and outputs a folder containing .JPG images next to the .HEIC images.
 
 library(magick)
-library(rstudioapi)   # provides function to let user pick directory
 library(svDialogs)   # provides function to construct dialog boxes for GUI
 
-# -1- Ask user to pick the folder  --------------------------------------------------------
-wd <- selectDirectory(caption = "Select the folder containing your chosen images:")
+
+# -1- Ask user to pick the folder --------------------------------------------------------------
+wd <- dlg_dir(title = "Select the folder containing your chosen images")$res
 setwd(wd)
 
 
-# -2- Allow user to select HEIC or PNG to convert  ----------------------------------------
+# -2- Allow user to select HEIC or PNG to convert ----------------------------------------------
 png_or_heic <- dlg_list(c("HEIC", "PNG"), title = "Choose format to convert to JPG")$res
 
 
-###########################################################################################
-
-if (png_or_heic == "HEIC") {
-  cat("You chose HEIC\n")
-  
-  # -2.1- Get all HEIC files --------------------------------------------------------------
-  heic_files <- list.files(pattern = "\\.HEIC$", ignore.case = TRUE)
+# -2.1- Create a function which combines the selected images and creates an output folder ------
+convert_all <- function(ext, output_folder) {
+  files <- list.files(pattern = paste0("\\.", ext, "$"), ignore.case = TRUE)
   
   
-  # -2.2- Define and create output folder -------------------------------------------------
-  output_folder <- "HEIC_to_JPG"
+  # Create an output folder with the converted images if it does not exist
   if (!dir.exists(output_folder)) dir.create(output_folder)
   
   
-  # -2.3- Function to convert one file ----------------------------------------------------
+  # -2.2- Create a function which converts an image to JPG -------------------------------------
   convert_to_jpg <- function(file) {
-    img <- image_read(file)                        # Read HEIC
-    img_jpg <- image_convert(img, format = "jpg")  # Convert to JPG
+    img <- image_read(file)
+    img_jpg <- image_convert(img, format = "jpg")
     jpg_file <- file.path(output_folder, paste0(tools::file_path_sans_ext(file), ".jpg"))
-    image_write(img_jpg, path = jpg_file)          # Write JPG
+    image_write(img_jpg, path = jpg_file)
     cat("Converted:", file, "->", jpg_file, "\n")
   }
   
   
-  # -2.4- Apply function to all HEIC files ------------------------------------------------
-  lapply(heic_files, convert_to_jpg)
-  
-  cat("All files converted!\n")
-  
-###########################################################################################
-  
-} else if (png_or_heic == "PNG") {
-  cat("You chose PNG\n")
-  
-  # -2.1- Get all .PNG files --------------------------------------------------------------
-  png_files <- list.files(pattern = "\\.PNG$", ignore.case = TRUE)
-  
-  
-  # -2.2- Define and create output folder -------------------------------------------------
-  output_folder <- "PNG_to_JPG"
-  if (!dir.exists(output_folder)) dir.create(output_folder)
-  
-  
-  # -2.3- Function to convert one file ----------------------------------------------------
-  convert_to_jpg <- function(file) {
-    img <- image_read(file)                        # Read PNG
-    img_jpg <- image_convert(img, format = "jpg")  # Convert to JPG
-    jpg_file <- file.path(output_folder, paste0(tools::file_path_sans_ext(file), ".jpg"))
-    image_write(img_jpg, path = jpg_file)          # Write JPG
-    cat("Converted:", file, "->", jpg_file, "\n")
-  }
-  
-  
-  # -2.4- Apply function to all HEIC files ------------------------------------------------
-  lapply(png_files, convert_to_jpg)
-  
-  cat("All files converted!\n")
-  
-###########################################################################################
-  
-} else {
-  cat("Invalid input. Please enter 'HEIC' or 'PNG'.\n")
+  # -2.3- Apply the convert_to_jpg function to each image in 'files' ---------------------------
+  lapply(files, convert_to_jpg)
+  cat("All", toupper(ext), "files converted!\n")
 }
+
+
+# -3- Apply the convert_all function based from file ext of choice made by user ----------------
+if (png_or_heic == "HEIC") {
+  convert_all("HEIC", "HEIC_to_JPG")
+
+} else if (png_or_heic == "PNG") {
+  convert_all("PNG", "PNG_to_JPG")
+  
+} else
+  cat("Invalid input. Please run the code again.\n")
